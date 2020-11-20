727d544658f833a48ccc766c68c5dcd3cf4019bd
Gabriel Koehler <gabriel.koehler@hotmail.com>
2020-11-20 19:31:39 -0300

    Finalizacao da aplicação web

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
+  delete "produtos/:id", to: "produtos#destroy", as: :produto
+  post "produtos", to: "produtos#create"
+  get "produtos/new", to: "produtos#new"
+
+  root to: "produtos#index"
+end
+
